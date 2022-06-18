872d531749c77887b2ee8f0c34bea6e0167393a2
Christian <xian04@yahoo.com>
2022-06-18 15:20:23 -0700

    Added eslint and configuration Installed Next eslint plugin Installed missing plugin

--- pages/login/employee.js
+++ pages/login/employee.js
@@ -1,19 +1,16 @@
 import Image from "next/image"; // Image supports optimization by default, and works with modern formats like .webp (stills and animations) and .apng (animations)
 
 export default function Employee() {
-  const ProfilePic = () => {
-    <Image
-      src="../../public/images/unknown-3.png"
-      height={400} // desired size, may change with optimizations
-      width={400}
-      alt="Profile Pic"
-    />;
-  };
-
   return (
     <>
       <h1>Employee Page</h1>
-      <ProfilePic />
+      <Image
+        src="../../public/images/unknown-3.png"
+        height={400} // desired size, may change with optimizations
+        width={400}
+        alt="Profile Pic"
+      />
+      ;
     </>
   );
 }
